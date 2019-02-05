fc74ba8305805f881795e7295046a5b2b7ff5126
Caleb Spindler <caleb.spindler@gmail.com>
2019-02-04 23:56:59 -0500

    removed unnecessary div from header and adding CSS to home page

--- src/components/Header.js
+++ src/components/Header.js
@@ -8,15 +8,13 @@ import {
 
 // Header Component with blog name, slogan, and nav
 const Header = props =>
-  <header id="header" className="container">
-    <div id="branding" className="container">
+  <header className="container">
       <CloudinaryContext cloudName="spindler-creative">
         <Image publicId="logo.png" >
           <Transformation height="50" crop="scale" />
         </Image>
       </CloudinaryContext>
       <h1 id="brandName"><NavLink exact to="/">Spindler Creative</NavLink></h1>
-    </div>
   </header>
 
   export default Header;
