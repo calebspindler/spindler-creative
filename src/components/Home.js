a6be99d5bd568fb0a95cef3b6018423e42489e49
Caleb Spindler <caleb.spindler@gmail.com>
2019-02-04 20:28:40 -0500

    added about copy and mobile nav

--- src/components/Home.js
+++ src/components/Home.js
@@ -7,12 +7,22 @@ import {
 
 // Contact Component with a clickable email address.
 const Home = () => (
-  <div className="main-content fadeIn">
-    <div id="home">
+  <div className="main-content fadeIn" id="home">
+    <div id="about">
       <div id="rectangle"></div>
       <CloudinaryContext cloudName="spindler-creative">
-        <Image id="aboutImage" publicId="about-image.jpg" />
+        <Image id="aboutImage" publicId="about-image.jpg">
+          <Transformation width="800" crop="scale" />
+        </Image>
       </CloudinaryContext>
+    </div>
+    <div id="aboutCopy">
+      <p>Hi, I'm Caleb Spindler! I am a web designer, developer, and content creator currently living in Louisville, KY.</p>
+      <p>When I’m not designing or developing websites, you’ll probably find me hanging with my fam, and we’ll likely be drinking coffee, hiking, or eating tacos.</p>
+    </div>
+    <div id="mobileNav">
+      <button>Recent Work</button>
+      <button>Contact</button>
     </div>
   </div>
 );
