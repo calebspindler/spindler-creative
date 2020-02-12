a4016d5f6bb4a907516e0b33c8f7e4e661794b61
Caleb Spindler <caleb.spindler@gmail.com>
2020-02-11 16:24:33 -0800

    changed onclick to addeventlistener

--- public/vanilla-js/js/calculateTipSplit.js
+++ public/vanilla-js/js/calculateTipSplit.js
@@ -13,49 +13,45 @@ function calculateTip(billTotal = parseFloat(document.getElementById('beforeTax'
     document.getElementById("splitBillBtn").style.display = "block";
 }
 
-document.getElementById("splitQuantity").onclick = function() {
-    document.getElementById("splitQuantity").value = "";
-};
+// Removes the value so that the user can enter however many they want.
+document.querySelector("#splitQuantity").addEventListener('click', function() {
+    document.querySelector("#splitQuantity").value = "";
+});
 
-// Run calculateTip app when 'Crunch!' button is clicked. Show an alert box if nothing or 0 is entered.
-document.getElementById("crunchBill").onclick = function() {
-    if (document.getElementById('beforeTax').value === '' || document.getElementById('beforeTax').value === "0" ) {
+// Run calculateTip app when "Crunch!" button is clicked. Show an alert box if nothing or 0 is entered.
+document.querySelector("#crunchBill").addEventListener('click', function() {
+    if (document.getElementById('beforeTax').value === "" || document.getElementById('beforeTax').value === "0" ) {
         alert("Please enter a valid number.");
     } else {
         calculateTip();
         document.getElementById("resetCalculateApp").style.display = "block";
     }
-};
+});
 
-// Show split bill input when 'Need to split?' is clicked. Default to two people.
-document.getElementById("splitBillBtn").onclick = function() {
+// Show split bill input when "Need to split?" is clicked.
+document.querySelector('#splitBillBtn').addEventListener('click', function() {
     document.getElementById("splitBillQuantity").style.display = "flex";
-};
+});
 
 // Input division for split payments on front end
 function splitBill(totalWithTip = parseFloat(document.getElementById("totalWithTip").innerHTML), people = parseInt(document.getElementById('splitQuantity').value)) {
-    console.log(totalWithTip / people);
     let totalPerPerson = totalWithTip / people;
     totalPerPerson = totalPerPerson.toFixed(2);
     document.getElementById("totalPerPerson").innerHTML = totalPerPerson;
 }
 
-// Run splitBill app when Split! button is clicked.
-document.getElementById("splitBill").onclick = function() {
+// Run splitBill app when "Split!" button is clicked.
+document.querySelector("#splitBill").addEventListener('click', function() {
     splitBill();
-};
+});
 
 // Reset button for resetting entire app
-document.getElementById("resetButton").onclick = function() {
-    // Hides all sections
+document.querySelector('#resetButton').addEventListener('click', function() {
     document.getElementById("splitBillBtn").style.display = "none";
     document.getElementById("totalWithTipContainer").style.display = "none";
     document.getElementById("splitBillQuantity").style.display = "none";
     document.getElementById("resetCalculateApp").style.display = "none";
-    //Removes values entered for "How much was your bill?" input.
-    document.getElementById("beforeTax").value = "";
-    // Not perfect. This removes even the "Choose an option" text.
-    document.getElementById("serviceQuality").value = "";
-};
+    document.getElementById("calculateTipForm").reset();
+});
 
 // Not perfect, because it rounds, if the total is an odd number. If the total is 201.27 and split between two people, it shows 100.64, which is 201.28.
